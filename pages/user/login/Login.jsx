261a0dc61d0b44727473d09a99f6f57947640392
Fatima Noble <fatinobleg@gmail.com>
2023-07-05 22:16:29 -0300

    Token

--- pages/user/login/Login.jsx
+++ pages/user/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Layout from '../../../src/components/UserLayout';
 import { useState, useRef } from "react";
 import { Container, Typography, TextField, Button, Box } from '@mui/material';
 import { findUserByMail } from '../../../services/userService';
@@ -7,7 +8,7 @@ import Alert from '@mui/material/Alert';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const Register = () => {
+const Login = () => {
     const [mail, setMail] = useState("");
     const [password, setPassword] = useState("");
     const [open, setOpen] = useState(false);
@@ -51,6 +52,7 @@ const Register = () => {
         try {
             const response = await findUserByMail(data);
             if (response.statusText == "OK") {
+                localStorage.setItem('token login', response.data.token);
                 handleMessage("Login correcto, bienvenid@ " + response.data.name + "!", "success");
                 router.push('/admin/recipes') //ToDo redirigir a catalogo
             } else {
@@ -63,93 +65,95 @@ const Register = () => {
     }
 
     return (
-        <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <img src="/images/muerde_logo_small.png" alt="Muerde logo" />
-                <Typography component="h1" variant="h5" sx={{ color: '#7B3E19' }}>
-                    Ingresar
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>                    
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="mail"
-                        label="Correo Electrónico"
-                        name="mail"
-                        autoComplete="mail"
-                        autoFocus
-                        onChange={e => setMail(e.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Contraseña"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                    <Link href="/user/register" passHref>
-                        <Typography
-                            variant="body2"
-                            align="center"
-                            sx={{ cursor: 'pointer', textDecoration: 'underline', mt: 2, color: '#7B3E19'}}
-                        >
-                            ¿Aún no eres cliente? Registrate aquí
-                        </Typography>
-                    </Link>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{
-                            display: 'block',
-                            mt: 2,
-                            ml: 'auto',
-                            mr: 'auto',
-                            backgroundColor: '#EDCBA2',
-                            color: '#7B3E19',
-                            '&:hover': {
-                                backgroundColor: '#CCA870',
-                            },
-                        }}
-                    >
+        <Layout>
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <img src="/images/muerde_logo_small.png" alt="Muerde logo" />
+                    <Typography component="h1" variant="h5" sx={{ color: '#7B3E19' }}>
                         Ingresar
-                    </Button>
-                    <Popover
-                        open={open}
-                        anchorEl={anchorRef.current}
-                        onClose={handleClose}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'center',
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'center',
-                        }}
-                        style={{ transform: 'translateY(10px)' }}
-                    >
-                        <Alert severity={messageType} sx={{ width: '100%' }}>
-                            {message}
-                        </Alert>
-                    </Popover>
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>                    
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="mail"
+                            label="Correo Electrónico"
+                            name="mail"
+                            autoComplete="mail"
+                            autoFocus
+                            onChange={e => setMail(e.target.value)}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Contraseña"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                        <Link href="/user/register" passHref>
+                            <Typography
+                                variant="body2"
+                                align="center"
+                                sx={{ cursor: 'pointer', textDecoration: 'underline', mt: 2, color: '#7B3E19'}}
+                            >
+                                ¿Aún no eres cliente? Registrate aquí
+                            </Typography>
+                        </Link>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{
+                                display: 'block',
+                                mt: 2,
+                                ml: 'auto',
+                                mr: 'auto',
+                                backgroundColor: '#EDCBA2',
+                                color: '#7B3E19',
+                                '&:hover': {
+                                    backgroundColor: '#CCA870',
+                                },
+                            }}
+                        >
+                            Ingresar
+                        </Button>
+                        <Popover
+                            open={open}
+                            anchorEl={anchorRef.current}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            style={{ transform: 'translateY(10px)' }}
+                        >
+                            <Alert severity={messageType} sx={{ width: '100%' }}>
+                                {message}
+                            </Alert>
+                        </Popover>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        </Layout>
     );
 }
 
-export default Register;
+export default Login;
