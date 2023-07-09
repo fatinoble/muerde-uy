3c833185c4aa27d42dd071383ae2fc758fc229da
Fatima Noble <fatinobleg@gmail.com>
2023-07-09 14:49:13 -0300

    User table

--- pages/admin/clients/Clients.jsx
+++ pages/admin/clients/Clients.jsx
@@ -1,7 +1,7 @@
 import Layout from '../../../src/components/AdminLayout';
 import { getUsers } from '../../../services/userService';
 import { useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import React, { useState } from "react";
 
 const Clients = () => {
@@ -14,39 +14,47 @@ const Clients = () => {
       });
   }, []);
 
-  console.log(users);
+  const StyledTableContainer = styled(TableContainer)({
+    backgroundColor: 'white',
+    marginTop: '20px',
+  });
+
+  const StyledTableCell = styled(TableCell)({
+    color: 'brown',
+    fontWeight: 'bold',
+  });
 
   return (
     <Layout>
-    <TableContainer>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Id</TableCell>
-          <TableCell>Nombre</TableCell>
-          <TableCell>Mail</TableCell>
-          <TableCell>Teléfono</TableCell>
-          <TableCell>Dirección</TableCell>
-          <TableCell>Fecha de registro</TableCell>
-          <TableCell>Rol</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {users.map((user) => (
-          <TableRow key={user.id_user}>
-            <TableCell>{user.id_user}</TableCell>
-            <TableCell>{user.name}</TableCell>
-            <TableCell>{user.mail}</TableCell>
-            <TableCell>{user.phone}</TableCell>
-            <TableCell>{user.address}</TableCell>
-            <TableCell>{user.creation_date}</TableCell>
-            <TableCell>{user.role}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
-     </Layout>
+      <StyledTableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Id</StyledTableCell>
+              <StyledTableCell>Nombre</StyledTableCell>
+              <StyledTableCell>Mail</StyledTableCell>
+              <StyledTableCell>Teléfono</StyledTableCell>
+              <StyledTableCell>Dirección</StyledTableCell>
+              <StyledTableCell>Fecha de registro</StyledTableCell>
+              <StyledTableCell>Rol</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id_user}>
+                <TableCell>{user.id_user}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.mail}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.address}</TableCell>
+                <TableCell>{user.creation_date}</TableCell>
+                <TableCell>{user.role}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledTableContainer>
+    </Layout>
   );
 };
 
