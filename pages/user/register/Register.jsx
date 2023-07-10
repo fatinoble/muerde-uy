20f4970e817eab30569b051cb1a95fc82c42f2da
Fatima Noble <fatinobleg@gmail.com>
2023-07-10 18:26:53 -0300

    Refactor register form

--- pages/user/register/Register.jsx
+++ pages/user/register/Register.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Layout from '../../../src/components/UserLayout';
 import { useState, useRef } from "react";
 import { Container, Typography, TextField, Button, Box } from '@mui/material';
 import { createUser } from '../../../services/userService';
 import Popover from '@mui/material/Popover';
 import Alert from '@mui/material/Alert';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+import Grid from '@mui/material/Grid';
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -80,131 +79,253 @@ const Register = () => {
         }
     }
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        router.push("/user/login");
+    };
+
     return (
-        <Layout>
-            <Container component="main" maxWidth="xs">
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <img src="/images/muerde_logo_small.png" alt="Muerde logo" />
-                    <Typography component="h1" variant="h5" sx={{ color: '#7B3E19' }}>
-                        Registrarse
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="name"
-                            label="Nombre"
-                            name="name"
-                            autoComplete="name"
-                            autoFocus
-                            onChange={e => setName(e.target.value)}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="mail"
-                            label="Correo Electrónico"
-                            name="mail"
-                            autoComplete="mail"
-                            autoFocus
-                            onChange={e => setMail(e.target.value)}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Contraseña"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="address"
-                            label="Dirección"
-                            name="address"
-                            autoComplete="address"
-                            autoFocus
-                            onChange={e => setAddress(e.target.value)}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="phone"
-                            label="Teléfono"
-                            name="phone"
-                            autoComplete="phone"
-                            autoFocus
-                            onChange={e => setPhone(e.target.value)}
-                        />
-                        <Link href="/user/login" passHref>
-                            <Typography
-                                variant="body2"
-                                align="center"
-                                sx={{ cursor: 'pointer', textDecoration: 'underline', mt: 2, color: '#7B3E19' }}
+        <Grid container direction="row">
+            <Grid item xs={6} sx={{ backgroundColor: '#e28d8d' }}>
+                <Container component="main" maxWidth="xs">
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'flex-start',
+                        }}
+                    >
+                        <img src="/images/muerde_logo_small.png" alt="Muerde logo" />
+                        <Typography component="h1" variant="h5" sx={{ color: '#fff', fontSize: '60px', textAlign: 'left', fontFamily: 'Poppins, sans-serif', fontWeight: 'bold', mt: 'auto' }}>
+                            Regístrate
+                        </Typography>
+                        <Typography component="h1" variant="h5" sx={{ color: '#fff', fontSize: '25px', textAlign: 'left', fontFamily: 'Poppins, sans-serif' }}>
+                            para comenzar a encargar tortas deliciosas
+                        </Typography>
+                        <Typography variant="body2" align="left" sx={{ mt: 2, color: '#fff', textAlign: 'left', fontFamily: 'Poppins, sans-serif' }}>
+                            ¿Ya eres cliente? {' '}
+                            <a
+                                href="/user/login"
+                                onClick={handleClick}
+                                style={{ cursor: 'pointer', textDecoration: 'none', color: '#A04242', textAlign: 'left', fontFamily: 'Poppins, sans-serif' }}
                             >
-                                ¿Ya tienes una cuenta? Inicia sesión aquí
-                            </Typography>
-                        </Link>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{
-                                display: 'block',
-                                mt: 2,
-                                ml: 'auto',
-                                mr: 'auto',
-                                backgroundColor: '#EDCBA2',
-                                color: '#7B3E19',
-                                '&:hover': {
-                                    backgroundColor: '#CCA870',
-                                },
-                            }}
-                        >
-                            Registrarse
-                        </Button>
-                        <Popover
-                            open={open}
-                            anchorEl={anchorRef.current}
-                            onClose={handleClose}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'center',
-                            }}
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'center',
-                            }}
-                            style={{ transform: 'translateY(10px)' }}
-                        >
-                            <Alert severity={messageType} sx={{ width: '100%' }}>
-                                {message}
-                            </Alert>
-                        </Popover>
+                                Inicia sesión aquí
+                            </a>
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                sx={{
+                                    backgroundColor: '#D88282',
+                                    borderRadius: '13px',
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                    },
+                                    '&:hover': {
+                                        boxShadow: '0 0 10px rgba(0,0,0,0.25)',
+                                    },
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="name"
+                                InputProps={{
+                                    placeholder: 'Nombre',
+                                    style: { color: '#fff' }
+                                }}
+                                name="name"
+                                autoComplete="name"
+                                autoFocus
+                                onChange={e => setName(e.target.value)}
+                            />
+                            <TextField
+                                sx={{
+                                    backgroundColor: '#D88282',
+                                    borderRadius: '13px',
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                    },
+                                    '&:hover': {
+                                        boxShadow: '0 0 10px rgba(0,0,0,0.25)',
+                                    },
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="mail"
+                                InputProps={{
+                                    placeholder: 'Email',
+                                    style: { color: '#fff' }
+                                }}
+                                name="mail"
+                                autoComplete="mail"
+                                autoFocus
+                                onChange={e => setMail(e.target.value)}
+                            />
+                            <TextField
+                                sx={{
+                                    backgroundColor: '#D88282',
+                                    borderRadius: '13px',
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                    },
+                                    '&:hover': {
+                                        boxShadow: '0 0 10px rgba(0,0,0,0.25)',
+                                    },
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                InputProps={{
+                                    placeholder: 'Contraseña',
+                                    style: { color: '#fff' }
+                                }}
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                            <TextField
+                                sx={{
+                                    backgroundColor: '#D88282',
+                                    borderRadius: '13px',
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                    },
+                                    '&:hover': {
+                                        boxShadow: '0 0 10px rgba(0,0,0,0.25)',
+                                    },
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="address"
+                                InputProps={{
+                                    placeholder: 'Dirección',
+                                    style: { color: '#fff' }
+                                }}
+                                name="address"
+                                autoComplete="address"
+                                autoFocus
+                                onChange={e => setAddress(e.target.value)}
+                            />
+                            <TextField
+                                sx={{
+                                    backgroundColor: '#D88282',
+                                    borderRadius: '13px',
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: 'transparent',
+                                        },
+                                    },
+                                    '&:hover': {
+                                        boxShadow: '0 0 10px rgba(0,0,0,0.25)',
+                                    },
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="phone"
+                                InputProps={{
+                                    placeholder: 'Teléfono',
+                                    style: { color: '#fff' }
+                                }} name="phone"
+                                autoComplete="phone"
+                                autoFocus
+                                onChange={e => setPhone(e.target.value)}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{
+                                    height: '50px',
+                                    borderRadius: '13px',
+                                    display: 'block',
+                                    mt: 2,
+                                    ml: 'auto',
+                                    mr: 'auto',
+                                    backgroundColor: '#A95C5C',
+                                    color: '#fff',
+                                    fontFamily: 'Poppins, sans-serif',
+                                    textTransform: 'capitalize',
+                                    '&:hover': {
+                                        backgroundColor: '#A95C5C',
+                                    },
+                                }}
+                            >
+                                Registrarse
+                            </Button>
+                            <Popover
+                                open={open}
+                                anchorEl={anchorRef.current}
+                                onClose={handleClose}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'center',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                }}
+                                style={{ transform: 'translateY(10px)' }}
+                            >
+                                <Alert severity={messageType} sx={{ width: '100%' }}>
+                                    {message}
+                                </Alert>
+                            </Popover>
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
-        </Layout>
+                </Container>
+            </Grid>
+            <Grid item xs={6}>
+                <img src="/images/img_login.svg" alt="Imagen de una torta" style={{ width: '100%', height: '100vh', objectFit: 'cover' }} />
+            </Grid>
+        </Grid>
     );
 }
 
