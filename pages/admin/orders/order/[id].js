2f8e44da6b71c84e34569de5364263230c64602d
NicolasSeoane <nicolas97seoane@gmail.com>
2023-08-28 19:18:23 -0300

    fixes

--- pages/admin/orders/order/[id].js
+++ pages/admin/orders/order/[id].js
@@ -2,13 +2,14 @@ import axios from 'axios';
 import Layout from '../../../../src/components/AdminLayout';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { Container, Grid, List, ListItem, ListItemText, MenuItem, Paper, Select, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material';
-import { getOrderStateName } from '@/utils';
+import { Container, Grid, List, ListItem, ListItemText, MenuItem, Paper, Select, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Tooltip } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import { getOrderPaymentMethodName, getOrderStateName } from '@/utils';
 
 function OrderScreen() {
 
-    
-  const router = useRouter();
+
+    const router = useRouter();
 
     const { query } = useRouter();
     const orderId = query.id;
@@ -22,11 +23,13 @@ function OrderScreen() {
     const [ordersState, setOrdersState] = useState([]);
     const [openStateModal, setOpenStateModal] = useState(false);
     const [selectedState, setSelectedState] = useState('');
+    const [fetchedOrderState, setFetchedOrderState] = useState('');
 
 
     useEffect(() => {
         fetchOrder();
         fetchOrderState();
+        
     }, [])
 
     const fetchOrder = async () => {
@@ -38,6 +41,7 @@ function OrderScreen() {
             });
             const data = response.data;
             setOrder(data);
+            setFetchedOrderState(data.status);
         } catch (error) {
             console.error('Error fetching sale:', error);
         }
@@ -78,31 +82,43 @@ function OrderScreen() {
 
 
     return (
-        
+
         <Layout>
             <Container style={{ marginTop: '16px', marginBottom: '16px' }} maxWidth="lg">
-            <Button style={{marginBottom: '20px'}}variant="contained" onClick={(e) => {
-                        e.preventDefault();
-                        router.push(`/admin/orders`);
-                      }}>
-                        Volver
-                      </Button>
+                <Button style={{ marginBottom: '20px' }} variant="contained" onClick={(e) => {
+                    e.preventDefault();
+                    router.push(`/admin/orders`);
+                }}>
+                    Volver
+                </Button>
                 <Paper style={{ padding: '25px' }}>
                     <Typography variant="h3" gutterBottom>
                         Orden Nro. {order?.id_sale}
                     </Typography>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
-                            <Typography style={{marginBottom: '20px'}}variant="h6">Pedido ID: {order?.id_sale}</Typography>
-                            <Typography style={{marginBottom: '20px'}}variant="h6">Fecha de orden realizada: {order?.start_date?.substring(0, 10)}</Typography>
-                            {order.finish_date && <Typography style={{marginBottom: '20px'}}variant="h6">Fecha finalizado: {order?.finish_date?.substring(0, 10)}</Typography>}
-                            <Typography style={{marginBottom: '20px'}}variant="h6">Nombre usuario: {order?.user ? order?.user?.name : 'Usuario eliminado'}</Typography>
-                            <Typography style={{marginBottom: '20px'}}variant="h6">Mail usuario: {order?.user ? order.user?.mail : 'Usuario eliminado'}</Typography>
-                            <Typography style={{marginBottom: '20px'}}variant="h6">Tipo de pedido: {order?.delivery_type}</Typography>
-                            {order.delivery_type == "DELIVERY" && <Typography style={{marginBottom: '20px'}}variant="h6">Direccion de envio: {order?.user?.address}</Typography>}
-                            <Typography style={{marginBottom: '20px'}}variant="h6"> Estado:
-                                <Select style={{marginBottom: '20px'}}
-                                    value={order?.status}
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Pedido ID: {order?.id_sale}</Typography>
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Fecha de orden realizada: {order?.start_date?.substring(0, 10)}</Typography>
+                            {order?.finish_date && <Typography style={{ marginBottom: '20px' }} variant="h6">Fecha finalizado: {order?.finish_date?.substring(0, 10)}</Typography>}
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Nombre usuario: {order?.user ? order?.user?.name : 'Usuario eliminado'}</Typography>
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Mail usuario: {order?.user ? order?.user?.mail : 'Usuario eliminado'}</Typography>
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Tipo de pedido: {order?.delivery_type == 'DELIVERY' ? 'Delivery' : 'Retiro en tienda'}</Typography>
+                            {order?.delivery_type == "DELIVERY" && <Typography style={{ marginBottom: '20px' }} variant="h6">Direccion de envio: {order?.user?.address}</Typography>}
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Metodo de pago:
+                                {order?.payment_method === "CASH"
+                                    ?
+                                    <span> {getOrderPaymentMethodName(order?.payment_method)}</span>
+                                    :
+                                    <span style={{ color: order?.transfer_number === null ? 'red' : 'green' }}> {getOrderPaymentMethodName(order?.payment_method)}
+                                        <Tooltip title={order?.transfer_number == null ? "Aún no se realizó la transferencia" : "Nro. transferencia: " + order?.transfer_number}>
+                                            <InfoIcon style={{ color: 'rgb(216, 130, 130)' }} />
+                                        </Tooltip>
+                                    </span>
+                                }
+                            </Typography>
+                            <Typography style={{ marginBottom: '20px' }} variant="h6">Estado:
+                                <Select style={{ marginBottom: '20px' }}
+                                    value={fetchedOrderState}
 
                                     onChange={(e) => {
                                         if (e.target.value != order?.status) {
@@ -115,10 +131,12 @@ function OrderScreen() {
                                     variant="outlined"
                                 >
                                     {ordersState?.map((orderState) => {
-                                        if((orderState != 'DONE_DELIVERY' && order.delivery_type != 'DELIVERY') || (orderState != 'DONE_PICK_UP' && order.delivery_type != 'PICK_UP')){
-                                            return <MenuItem key={orderState} value={orderState} autoFocus={order.status == orderState} selected={order.status == orderState}>
-                                                        {getOrderStateName(orderState)}
-                                                    </MenuItem>
+                                        if ((orderState != 'DONE_DELIVERY' && order?.delivery_type != 'DELIVERY') || (orderState != 'DONE_PICK_UP' && order?.delivery_type != 'PICK_UP')) {
+                                            return (
+                                                <MenuItem key={orderState} value={orderState} autoFocus={order?.status == orderState} selected={order?.status === orderState}>
+                                                    {getOrderStateName(orderState)}
+                                                </MenuItem>
+                                            );
                                         }
                                     })}
                                 </Select>
