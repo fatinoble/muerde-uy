c6cb5b99ca27f3264cbb3f7dd6bb211c0894a97b
NicolasSeoane <nicolas97seoane@gmail.com>
2023-08-29 18:18:40 -0300

    fix/ user menu and admin-user dropdown

--- src/components/AdminSettingsMenu/AdminSettingsMenu.jsx
+++ src/components/AdminSettingsMenu/AdminSettingsMenu.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import {ArrowDown} from '../../svg'
+import ExitToApp  from '@mui/icons-material/ExitToApp';
+import BarChart from "@mui/icons-material/BarChart";
 
 const AdminSettingsMenu = () => {
   const [userName, setUserName] = useState('');
+  const router = useRouter();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   useEffect(() => {
     const localStorageUserName = localStorage.getItem('user_name');
@@ -13,13 +17,28 @@ const AdminSettingsMenu = () => {
     }
   }, []); 
 
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+  const handleNavigation = (route) => {
+    router.push(route);
+  };
+
   return(
     <div className="user-settings-menu-container">
       <span className="user-name-container">{userName}</span>
-      <IconButton className="user-icon">
+      <IconButton className="user-icon"  onClick={toggleDropdown}>
         <AccountCircle style={{border: '1px solid #FFF', borderRadius: '60px'}}/>
       </IconButton>
+      {dropdownVisible && (
+        <div className="dropdown-menu">
+          <div className="dropdown-item" onClick={() => handleNavigation('/admin/reports')}><BarChart /> Dashboard</div>
+          <div className="dropdown-item" onClick={() => handleNavigation('/admin/logout')}><ExitToApp /> Logout</div>
+        </div>
+      )}
     </div>
+
+    
   );
 };
 
