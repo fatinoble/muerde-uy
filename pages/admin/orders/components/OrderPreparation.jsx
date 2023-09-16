123fa3e12588ee8c4e2016a62233590c9077d3da
Valentina Zimberg <valentina.zimberg@mercadolibre.com>
2023-09-16 19:54:33 -0300

    warning suggestion

--- pages/admin/orders/components/OrderPreparation.jsx
+++ pages/admin/orders/components/OrderPreparation.jsx
@@ -7,7 +7,10 @@ import { getApiUrl } from '../../../../services/utils';
 const OrderPreparation = () => {
   const [orderPreparationSuggestions, setOrderPreparationSuggestions] = useState([]);
   const [preparationSuggestionDay, setPreparationSuggestionDay] = useState({});
+  const [totalOrderPreparationTimePerDay, setTotalOrderPreparationTimePerDay] = useState([]);
+  const [maxPreparationTimePerDayMinutesEnabled, setMaxPreparationTimePerDayMinutesEnabled] = useState(10000);
   const [suggestionDay, setSuggestionDay] = useState("");
+  const [orderMessage, setOrderMessage] = useState("");
 
   const today = new Date();
   const formattedToday = today.toISOString().split('T')[0];
@@ -25,10 +28,42 @@ const OrderPreparation = () => {
       setOrderPreparationSuggestions(data.order_preparation_suggestion_per_day);
       setPreparationSuggestionDay(data.order_preparation_suggestion_per_day[0]);
       setSuggestionDay(data.order_preparation_suggestion_per_day[0]?.day);
+
+      const totalOrderPreparationTimePerDaySet = calculateTotalPreparationTime(data);
+      setTotalOrderPreparationTimePerDay(totalOrderPreparationTimePerDaySet);
+      setMaxPreparationTimePerDayMinutesEnabled(data?.max_preparation_time_per_day_minutes_enabled);
+      handleUpdateOrderMessage(totalOrderPreparationTimePerDaySet, data.order_preparation_suggestion_per_day[0]?.day, data?.max_preparation_time_per_day_minutes_enabled);
     } catch (error) {
       console.error('Error fetching order preparation suggestions:', error);
     }
   };
+
+  const handleUpdateOrderMessage = (totalOrderPrepTime = [], day, maxPrepEnabledConfig) => {
+    const timeSet = totalOrderPrepTime.find(totalOrder => totalOrder.day === day);
+    if (timeSet && timeSet.total_preparation_time_minutes > maxPrepEnabledConfig) {
+      setOrderMessage(`Este día ya superó el máximo de tiempo de preparación de pedidos de ${formatTime(maxPrepEnabledConfig)}, por lo que se encuentra deshabilitado en el calendario de pedidos para los clientes.`);
+    } else {
+      setOrderMessage("");
+    }
+  };
+
+  const calculateTotalPreparationTime = (data = {}) => {
+    const result = [];
+
+    data.order_preparation_suggestion_per_day.forEach((day) => {
+      const totalPreparationTime = day.preparation_suggestions.reduce(
+        (sum, suggestion) => sum + suggestion.total_preparation_time_minutes,
+        0
+      );
+
+      result.push({
+        day: day.day,
+        total_preparation_time_minutes: totalPreparationTime,
+      });
+    });
+
+    return result;
+  }
 
 
 
@@ -36,6 +71,7 @@ const OrderPreparation = () => {
     setSuggestionDay(date);
     const orderPreparationSuggestion = orderPreparationSuggestions?.find((suggestion) => suggestion?.day === date) || {}
     setPreparationSuggestionDay(orderPreparationSuggestion);
+    handleUpdateOrderMessage(totalOrderPreparationTimePerDay, date, maxPreparationTimePerDayMinutesEnabled);
   }
 
   const formatTime = (minutes) => {
@@ -46,16 +82,18 @@ const OrderPreparation = () => {
     } else {
       const hours = Math.floor(minutes / 60);
       const restOfMinutes = minutes % 60;
-      return `${hours} horas ${restOfMinutes} minutos`;
+      const hoursText = hours > 1 ? "horas" : "hora";
+      const minutesText = restOfMinutes > 1 ? "minutos" : "minuto";
+      return `${hours} ${hoursText} ${restOfMinutes} ${minutesText}`;
     }
   };
-
 
   return (
     <div className="order-preparation-container">
       <div className="order-preparation-title-container">
         <span className="order-preparation-main-title">Sugerencia de preparación para el día: </span>
         <TextField
+          className="day-order-selector-textfield"
           label="Seleccione día"
           type="date"
           value={suggestionDay}
@@ -67,18 +105,25 @@ const OrderPreparation = () => {
             min: formattedToday,
           }}
         />
+        {orderMessage && (
+          <div className="info-container">
+            <span className="order-prep-message-info">{orderMessage}</span>
+          </div>
+        )}
       </div>
       {(!preparationSuggestionDay ||
         isObjectEmpty(preparationSuggestionDay) ||
         preparationSuggestionDay?.preparation_suggestions?.length === 0) &&
-        <span>Aún no hay pedidos para este día.</span>
+        <span className="order-prep-message">Aún no hay pedidos para este día.</span>
       }
+
       <div className="order-preparation-suggestions-container">
         {preparationSuggestionDay?.preparation_suggestions?.map((suggestion, index) => {
           return (
             <div className="suggestion-container">
               <div className='suggestion-title-container'>
-                <span className='suggestion-title'>Agrupación de preparación {index + 1}  - {formatTime(suggestion?.total_preparation_time_minutes)}</span>
+                <span className='suggestion-title'>Agrupación de preparación {index + 1}: </span>
+                <span className='suggestion-title-data'>{formatTime(suggestion?.total_preparation_time_minutes)}</span>
               </div>
 
               <div className="common-ingredients-container">
